49edb519fe1b7c675b3b54a9501549484477c9d0
PiotrKozieja <114917742+PiotrKozieja@users.noreply.github.com>
2022-11-13 21:20:08 +0100

    Delete bcex directory

--- bcex/ex1.py
+++
@@ -1,9 +0,0 @@
-import array as arr
-import random
-arr3 = arr.array('i',[7 for i in range(5)])
-arr4 = arr.array('i', [i for i in range(1,10)])
-arr5 = arr.array('i',[i*2 for i in range(1,10)])
-arr6 = arr.array('i',[random.randint(1,20) for i in range(10)])
-
-
-
