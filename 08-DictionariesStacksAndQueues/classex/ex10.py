099ca177ddf44e1adfe990f6a4ce23f32ef3011b
PiotrKozieja <114917742+PiotrKozieja@users.noreply.github.com>
2022-11-28 12:50:47 +0100

    Add files via upload

---
+++ 08-DictionariesStacksAndQueues/classex/ex10.py
@@ -0,0 +1,10 @@
+poland = {"population": 37}
+germany = {"population": 47}
+czech = {"population": 57}
+mexico = {"population": 67}
+candad= {"population": 77}
+
+arr = [poland, germany, czech, mexico, candad]
+for i in arr:
+    print(i.items())
+
---
+++ 08-DictionariesStacksAndQueues/classex/ex9.py
@@ -0,0 +1,10 @@
+phone = {
+    "name" : "samsung",
+    "model" : "s10",
+    "ram" : "32gb",
+    "size": 10,
+    "battery": 3600,
+    "age": 13
+}
+for i , j in phone.items():
+    print(i,"-",j)
